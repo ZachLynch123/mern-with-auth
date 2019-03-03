3f78af1034d5d998c6ec8c2acf4c66c5c250f5bc
Zach <Zachary.Lynch1337@gmail.com>
2019-03-03 11:34:49 -0800

    Login page checks for valid email+password

---
+++ validation/login.js
@@ -0,0 +1,28 @@
+const validator = require('validator');
+const isEmpty = require('is-empty');
+
+module.exports = validateResgisterInput = data => {
+    let errors = {};
+
+
+    data.email = !isEmpty(data.email) ? data.email : "";
+    data.password = !isEmpty(data.password) ? data.password : "";
+
+    // Email check
+    if (validator.isEmpty(data.email)) {
+        errors.email = "Email required";
+    } else if (!validator.isEmail(data.email)) {
+        errors.email = "Invalid Email"
+    }
+
+    // check password
+    if (validator.isEmpty(data.password)) {
+        errors.password = "Paasword required"
+    }
+
+    // return object of results from check
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
