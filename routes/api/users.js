ee44557f9639d70ccc43ae1940c8a507dd1bb456
Zach <Zachary.Lynch1337@gmail.com>
2019-03-04 15:44:06 -0800

    Created login endpoint

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,3 +47,59 @@ router.post('/register', (req, res) => {
         })
     })
 })
+
+// @Route POST api/users/login
+// @desc Login user and return JWT token
+// @access Public
+
+router.post("/login", (req, res) => {
+    // Validation
+
+    const { errors, isValid } = validateLoginInput(req.body);
+
+    if (!isValid) {
+        return res.status(400).json(errors)
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email }).then(user => {
+        // check if user exists
+        if(!user) {
+            return res.status(400).json({ emailNotFound: "email not found"});
+        }
+        // Check password
+
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                // Create JWT Payload
+                const payload = {
+                    id: user.id,
+                    name: user.name
+                };
+                // Sign token
+
+                jwt.sign (
+                    payload,
+                    keys.secretOrKey,
+                    {
+                        expiresIn: 31556926 
+                    },
+                    (e, token) => {
+                        res.json({
+                            success: true,
+                            token: "Bearer " + token
+                        });
+                    }
+                );
+
+            } else {
+                return res.status(400).json({ passwordIncorrect: "Password Incorrect. Try again" });
+            }
+        })
+    })
+
+})
+
+module.exports = router;
